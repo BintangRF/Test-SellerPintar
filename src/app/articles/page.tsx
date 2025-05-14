65ead2278d8ef751c887f258d22d42493e2dc2ed
bintang.rf <bintangrama777@gmail.com>
2025-05-14 19:09:30 +0700

    fix

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ArticlesWrapper from "@/components/articles/ArticlesWrapper";
 import HeroSection from "@/components/articles/HeroSection";
 import ListArticles from "@/components/articles/ListArticles";
@@ -6,8 +6,10 @@ import ListArticles from "@/components/articles/ListArticles";
 export default function Articles() {
   return (
     <ArticlesWrapper>
-      <HeroSection />
-      <ListArticles />
+      <Suspense fallback={<div>Loading...</div>}>
+        <HeroSection />
+        <ListArticles />
+      </Suspense>
     </ArticlesWrapper>
   );
 }
